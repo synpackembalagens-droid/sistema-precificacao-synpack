5a55d2b5d832deb32b6cddd8a2c9a474596cfcae
synpackembalagens-droid <synpackembalagens@gmail.com>
2026-02-14 11:42:02 -0300

    Create server.js
    Criação do servidor inicial Express

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "API do Sistema de Precificação Synpack rodando 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
